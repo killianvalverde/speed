d938afdb86d45f13a19f00b60b42f5c724bab459
Killian Valverde <KillianValverde@gmail.com>
2024-10-15 07:39:09 +0200

    src: filesystem: add directory_iteration feature

--- src/speed/filesystem/directory_iteration.cpp
+++ src/speed/filesystem/directory_iteration.cpp
@@ -21,7 +21,7 @@
  * @file        speed/filesystem/directory_iteration.cpp
  * @brief       directory_iteration class methods definition.
  * @author      Killian Valverde
- * @date        2018/11/25
+ * @date        2024/10/15
  */
 
 #include "../stringutils/stringutils.hpp"
@@ -32,74 +32,177 @@
 namespace speed::filesystem {
 
 
-//directory_iteration::iterator::iterator(directory_iteration* compo)
-//        : stck_()
-//        , vistd_inos_()
-//        , cur_pth_()
-//        , end_(false)
-//        , opend_(false)
-//        , compo_(compo)
-//{
-//    stck_.emplace();
-//    if (system::opendir(&stck_.top(), compo_->dir_pth_.c_str()))
-//    {
-//        opend_ = true;
-//        this->operator++();
-//    }
-//    else
-//    {
-//        end_ = true;
-//    }
-//}
+directory_iteration::const_iterator::const_iterator(const directory_iteration* composit)
+       : cur_dir_()
+       , cur_fle_()
+       , path_stck_()
+       , vistd_inos_()
+       , composit_(composit)
+       , end_(false)
+       , current_recursivity_levl_(0)
+{
+    if (composit == nullptr)
+    {
+        end_ = true;
+        return;
+    }
+
+    cur_dir_ = composit->root_pth_;
+    cur_fle_ = cur_dir_;
+
+    if (open_directory())
+    {
+        this->operator++();
+    }
+}
 
 
-//directory_iteration::iterator::~iterator()
-//{
-//    //if (opend_)
-//    //{
-//    //    system::closedir(&dir_ent_);
-//    //}
-//}
+directory_iteration::const_iterator::~const_iterator() noexcept
+{
+    while (!path_stck_.empty())
+    {
+        speed::system::filesystem::closedir(&path_stck_.top());
+        path_stck_.pop();
+    }
+
+    end_ = true;
+}
 
 
-//directory_iteration::iterator::self_type& directory_iteration::iterator::operator ++()
-//{
-//    //system::basic_directory_entity* cur_dir_ent = &stck_.top();
-//    //
-//    //if (speed::system::is_directory(cur_pth_.c_str()))
-//    //{
-//    //    stck_.emplace();
-//    //    cur_dir_ent = &stck_.top();
-//    //    system::opendir(cur_dir_ent, cur_pth_.c_str());
-//    //}
-//    //
-//    //do
-//    //{
-//    //    system::readdir(cur_dir_ent);
-//    //} while (cur_dir_ent.
-//    //        speed::stringutils::strcmp(cur_dir_ent->nme, ".") == 0 ||
-//    //         speed::stringutils::strcmp(cur_dir_ent->nme, "..") == 0);
-//    //
-//    //
-//    //while (!system::readdir(&dir_ent_))
-//    //{
-//    //    end_ = true;
-//    //}
-//}
+directory_iteration::const_iterator::self_type& directory_iteration::const_iterator::operator ++()
+{
+    directory_entity& cur_dir_ent = path_stck_.top();
+
+    if (!read_directory())
+    {
+        close_directory();
+
+        if (end_)
+        {
+            return *this;
+        }
+    }
+    else
+    {
+        cur_fle_.replace_filename(cur_dir_ent.nme);
+
+        if (speed::system::filesystem::is_directory(cur_fle_.c_str()))
+        {
+            cur_dir_ /= cur_dir_ent.nme;
+
+            if (open_directory())
+            {
+                return this->operator++();
+            }
+        }
+    }
+
+    if (!is_file_valid())
+    {
+        return this->operator++();
+    }
+
+    return *this;
+}
 
 
-//directory_iteration::directory_iteration(
-//        std::filesystem::path dir_pth,
-//        system::file_types fle_typs,
-//        std::uint64_t deep_lv,
-//        std::string regx_to_match
-//)
-//        : dir_pth_(std::move(dir_pth))
-//        , fle_typs_(fle_typs)
-//        , deep_lv_(deep_lv)
-//        , regx_(std::move(regx_to_match))
-//{
-//}
+bool directory_iteration::const_iterator::operator ==(const self_type& rhs) const noexcept
+{
+    auto lhs_ino = path_stck_.empty() ? ~0ull : path_stck_.top().ino;
+    auto rhs_ino = rhs.path_stck_.empty() ? ~0ull : rhs.path_stck_.top().ino;
+
+    if (end_ == rhs.end_)
+    {
+        return true;
+    }
+    if (composit_ != rhs.composit_ || lhs_ino != rhs_ino)
+    {
+        return false;
+    }
+
+    return true;
+}
+
+
+bool directory_iteration::const_iterator::open_directory()
+{
+    std::uint64_t ino = speed::system::filesystem::get_file_inode(cur_dir_.c_str());
+
+    if (current_recursivity_levl_ > composit_->recursivity_levl_ ||
+        vistd_inos_.contains(ino))
+    {
+        return false;
+    }
+
+    ++current_recursivity_levl_;
+    cur_fle_ /= ".";
+    path_stck_.emplace();
+
+    if (!speed::system::filesystem::opendir(&path_stck_.top(), cur_dir_.c_str()))
+    {
+        exit_directory();
+        return false;
+    }
+
+    vistd_inos_.emplace(ino);
+    return true;
+}
+
+
+bool directory_iteration::const_iterator::read_directory()
+{
+    bool succss;
+    directory_entity& cur_dir_ent = path_stck_.top();
+
+    do
+    {
+        succss = speed::system::filesystem::readdir(&cur_dir_ent);
+    }
+    while (succss && (speed::stringutils::strcmp(cur_dir_ent.nme, ".") == 0 ||
+                      speed::stringutils::strcmp(cur_dir_ent.nme, "..") == 0));
+
+    return succss;
+}
+
+
+void directory_iteration::const_iterator::close_directory()
+{
+    speed::system::filesystem::closedir(&path_stck_.top());
+    exit_directory();
+}
+
+
+void directory_iteration::const_iterator::exit_directory()
+{
+    path_stck_.pop();
+    cur_fle_ = cur_dir_;
+    cur_dir_ = cur_dir_.parent_path();
+    --current_recursivity_levl_;
+
+    if (path_stck_.empty())
+    {
+        end_ = true;
+    }
+}
+
+
+bool directory_iteration::const_iterator::is_file_valid()
+{
+    if (!std::regex_match(cur_fle_.filename().c_str(), composit_->regex_to_mtch_))
+    {
+        return false;
+    }
+    if (!speed::system::filesystem::is_file_type(cur_fle_.c_str(), composit_->file_typs_))
+    {
+        return false;
+    }
+    if (!speed::system::filesystem::access(cur_fle_.c_str(), composit_->access_mods_))
+    {
+        return false;
+    }
+
+    return true;
+}
 
 
 }
