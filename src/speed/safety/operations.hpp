789fc6b286743693548d9e82623af0a39bdd865b
Killian Valverde <KillianValverde@gmail.com>
2025-06-16 00:04:55 +0200

    refactor(safety): simplify variadic functions using folding expressions

--- src/speed/safety/operations.hpp
+++ src/speed/safety/operations.hpp
@@ -35,46 +35,19 @@
 
 namespace speed::safety {
 
-// TODO: Add support for signed integrals.
-/**
- * @brief       Fonction that allows to do an addition with overflow detection.
- * @param       trg : Integral to increment.
- * @param       val : Value to add to the integral.
- */
-template<typename TpTarget, typename TpValue>
-inline TpTarget add(
-        TpTarget trg,
-        const TpValue& val
-)
-{
-    constexpr TpTarget max_trg = std::numeric_limits<TpTarget>::max();
-    
-    if (trg > max_trg - static_cast<TpTarget>(val))
-    {
-        throw overflow_exception();
-    }
-    
-    return trg + static_cast<TpTarget>(val);
-}
-
 /**
  * @brief       Fonction that allows to do an addition with overflow detection.
  * @param       trg : Integral to increment.
  * @param       vals : Values to add to the integral.
  */
 template<typename TpTarget, typename... TpValues>
-inline TpTarget addl(
-        TpTarget trg,
-        const TpValues&... vals
-)
+inline TpTarget add(TpTarget trg, const TpValues&... vals)
 {
     constexpr TpTarget max_trg = std::numeric_limits<TpTarget>::max();
     
-    int dummy[sizeof...(TpValues)] = { (
-        (trg > max_trg - static_cast<TpTarget>(vals)) ?
-                throw overflow_exception() :
-                trg += static_cast<TpTarget>(vals), 0
-    )... };
+    ((trg > max_trg - static_cast<TpTarget>(vals)
+            ? throw overflow_exception()
+            : trg += static_cast<TpTarget>(vals)), ...);
     
     return trg;
 }
@@ -82,48 +55,20 @@ inline TpTarget addl(
 /**
  * @brief       Fonction that allows to do an addition with overflow detection.
  * @param       trg : Integral to increment.
- * @param       val : Value to add to the integral.
- * @return      If the function was successful true is returned, otherwise false is returned.
- */
-template<typename TpTarget, typename TpValue>
-inline bool try_add(
-        TpTarget* trg,
-        const TpValue& val
-) noexcept
-{
-    constexpr TpTarget max_trg = std::numeric_limits<TpTarget>::max();
-    bool scs = true;
-    
-    if (*trg > max_trg - static_cast<TpTarget>(val))
-    {
-        scs = false;
-    }
-    
-    *trg += static_cast<TpTarget>(val);
-    
-    return scs;
-}
-
-/**
- * @brief       Fonction that allows to do an addition with overflow detection.
- * @param       trg : Integral to increment.
  * @param       vals : Values to add to the integral.
  * @return      If the function was successful true is returned, otherwise false is returned.
  */
 template<typename TpTarget, typename... TpValues>
-inline bool try_addl(
-        TpTarget* trg,
-        const TpValues& ... vals
-) noexcept
+inline bool try_add(TpTarget* trg, const TpValues& ... vals) noexcept
 {
     constexpr TpTarget max_trg = std::numeric_limits<TpTarget>::max();
     bool scs = true;
     
-    int dummy[sizeof...(TpValues)] = { (
-        ((*trg > max_trg - static_cast<TpTarget>(vals)) ?
-                scs = false : 0),
-        *trg += static_cast<TpTarget>(vals), 0
-    )... };
+    ((scs
+            ? (*trg > max_trg - static_cast<TpTarget>(vals)
+                    ? scs = false
+                    : *trg += static_cast<TpTarget>(vals))
+            : 0), ...);
     
     return scs;
 }
@@ -132,24 +77,17 @@ inline bool try_addl(
  * @brief       Fonction that allows to do an addition with overflow detection and in the overflow
  *              case the maximum value of the type will be set to target.
  * @param       trg : Integral to increment.
- * @param       val : Values to add to the integral.
+ * @param       vals : Values to add to the integral.
  */
-template<typename TpTarget, typename TpValue>
-inline TpTarget addm(
-        TpTarget trg,
-        const TpValue& val
-) noexcept
+template<typename TpTarget, typename... TpValues>
+inline TpTarget addm(TpTarget trg, const TpValues&... vals) noexcept
 {
     constexpr TpTarget max_trg = std::numeric_limits<TpTarget>::max();
-    
-    if (trg > max_trg - static_cast<TpTarget>(val))
-    {
-        trg = max_trg;
-    }
-    else
-    {
-        trg += static_cast<TpTarget>(val);
-    }
+    bool saturatd = false;
+
+    ((saturatd || trg > max_trg - static_cast<TpTarget>(vals)
+            ? (trg = max_trg, saturatd = true)
+            : trg += static_cast<TpTarget>(vals)), ...);
     
     return trg;
 }
@@ -159,74 +97,22 @@ inline TpTarget addm(
  *              case the maximum value of the type will be set to target.
  * @param       trg : Integral to increment.
  * @param       vals : Values to add to the integral.
- */
-template<typename TpTarget, typename... TpValues>
-inline TpTarget addml(
-        TpTarget trg,
-        const TpValues&... vals
-) noexcept
-{
-    constexpr TpTarget max_trg = std::numeric_limits<TpTarget>::max();
-    
-    int dummy[sizeof...(TpValues)] = { (
-        (trg > max_trg - static_cast<TpTarget>(vals)) ?
-                trg = max_trg :
-                trg += static_cast<TpTarget>(vals), 0
-    )... };
-    
-    return trg;
-}
-
-/**
- * @brief       Fonction that allows to do an addition with overflow detection and in the overflow
- *              case the maximum value of the type will be set to target.
- * @param       trg : Integral to increment.
- * @param       val : Values to add to the integral.
  * @return      If the function was successful true is returned, otherwise false is returned.
  */
-template<typename TpTarget, typename TpValue>
+template<typename TpTarget, typename... TpValues>
 inline bool try_addm(
         TpTarget* trg,
-        const TpValue& val
-) noexcept
-{
-    constexpr TpTarget max_trg = std::numeric_limits<TpTarget>::max();
-    bool scs = true;
-    
-    if (*trg > max_trg - static_cast<TpTarget>(val))
-    {
-        *trg = max_trg;
-        scs = false;
-    }
-    else
-    {
-        *trg += static_cast<TpTarget>(val);
-    }
-    
-    return scs;
-}
-
-/**
- * @brief       Fonction that allows to do an addition with overflow detection and in the overflow
- *              case the maximum value of the type will be set to target.
- * @param       trg : Integral to increment.
- * @param       vals : Values to add to the integral.
- * @return      If the function was successful true is returned, otherwise false is returned.
- */
-template<typename TpTarget, typename... TpValues>
-inline bool try_addml(
-        TpTarget* trg,
         const TpValues& ... vals
 ) noexcept
 {
     constexpr TpTarget max_trg = std::numeric_limits<TpTarget>::max();
     bool scs = true;
     
-    int dummy[sizeof...(TpValues)] = { (
-        (*trg > max_trg - static_cast<TpTarget>(vals)) ?
-                *trg = max_trg, scs = false :
-                *trg += static_cast<TpTarget>(vals), 0
-    )... };
+    ((scs
+            ? (*trg > max_trg - static_cast<TpTarget>(vals)
+                    ? (*trg = max_trg, scs = false)
+                    : *trg += static_cast<TpTarget>(vals))
+            : 0), ...);
     
     return scs;
 }
